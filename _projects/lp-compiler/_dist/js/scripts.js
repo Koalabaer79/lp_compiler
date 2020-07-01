fa1ea4f607da652bb7b36eb9024a2aca0c90936b
Lars Urban <urban@u-php.de>
2020-07-01 08:50:39 +0200

    Optimized Scripts

--- _projects/lp-compiler/_dist/js/scripts.js
+++ _projects/lp-compiler/_dist/js/scripts.js
@@ -1,14 +1,121 @@
+// Easy Landing Page Programming by Lars Urban
+// Feel free to use and change the code
+
 console.log('Welcome to the Landingpage Compiler!');
+
+// Declare arrays and variables needed
+
+var elements = [ 'nav', 'header', 'description', 'mid1', 'features', 'mid2', 'commands' ];
+var sections = {description:'arrow1', mid1:'arrow2', features:'arrow3', mid2:'arrow4', commands:'arrow5'};
+var timeShow = null;
+var timeline = {};
+
+// Get URL-Parameter for intro start
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const intro = urlParams.get('intro');
+
+// Get Cookie 
+
+var myCookie = document.cookie.indexOf("intro=");
+
+// Set timeline for intro or not
+
+if (myCookie < 0 || intro === 'show') {
+    timeLine = [
+        {
+            id: "text_1",
+            anim: "fadeIn",
+            time: 200
+        },
+        {
+            id: "text_1",
+            anim: "fadeOut",
+            time: 2200
+        },
+        {
+            id: "text_2",
+            anim: "fadeIn",
+            time: 3200
+        },
+        {
+            id: "text_2",
+            anim: "fadeOut",
+            time: 5200
+        },
+        {
+            id: "text_3",
+            anim: "fadeIn",
+            time: 6200
+        },
+        {
+            id: "text_3",
+            anim: "fadeOut",
+            time: 8200
+        },
+        {
+            id: "background",
+            anim: "fadeOut",
+            time: 8200
+        },
+        {
+            id: "container",
+            anim: "display",
+            time: 9200
+        },
+        {
+            id: "background",
+            anim: "hide",
+            time: 9200
+        },
+    ];
+    var timeShow = 9201;
+    setCookie();
+}
+else {
+    timeLine = [
+        {
+            id: "container",
+            anim: "display",
+            time: 1
+        },
+        {
+            id: "background",
+            anim: "hide",
+            time: 1
+        },
+    ];
+    var timeShow = 2;
+}
+
+// Function if element is in fully in viewport ( on page load )
 
 function isInViewport(elem) {
     var bounding = elem.getBoundingClientRect();
     return (
         bounding.top >= 0 &&
         bounding.left >= 0 &&
-        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.bottom <= ((window.innerHeight) || document.documentElement.clientHeight) &&
         bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
+    
 };
+
+// Function if element is in partly in viewport ( when scrolling )
+
+function isElementXPercentInViewport (el, percentVisible) {
+    let
+      rect = el.getBoundingClientRect(),
+      windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+  
+    return !(
+      Math.floor(100 - (((rect.top >= 0 ? 0 : rect.top) / +-(rect.height / 1)) * 100)) < percentVisible ||
+      Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100) < percentVisible
+    )
+};
+
+// Function for animating Intro
 
 function animate(id,anim,time) {
     setTimeout(function(){
@@ -17,10 +124,21 @@ function animate(id,anim,time) {
     }, time);
 }
 
-var elements = [ 'nav', 'header', 'description', 'mid1', 'features', 'mid2', 'commands' ];
-var sections = {description:'arrow1', mid1:'arrow2', features:'arrow3', mid2:'arrow4', commands:'arrow5'};
+// Function to set intro-cookie ( expiring in 24 hours )
+
+function setCookie() {
+    var d = new Date();
+    d.setTime(d.getTime() + (1*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = "intro=shown; " + expires;
+}
+
+// If screen-width is bigger than 800 pixel
 
 if(window.innerWidth >= 800){
+
+    // Run fadeIn of fully visible elements after video
+
     setTimeout(function(){
         elements.forEach(el => {
             var elem = document.getElementById(el);
@@ -31,12 +149,14 @@ if(window.innerWidth >= 800){
                 }
             }
         });
-    }, 8201);
+    }, timeShow);
+
+    // Event listener on scroll to display hidden elements when 50% visible
 
     window.addEventListener("scroll", function(event) {
         elements.forEach(el => {
             var elem = document.getElementById(el);
-            if (isInViewport(elem)) {
+            if (isElementXPercentInViewport(elem, 50)) {
                 elem.classList.add('fadeIn');
                 if(sections[el] !== undefined) {
                     document.getElementById(sections[el]).classList.add('hidden');
@@ -45,48 +165,15 @@ if(window.innerWidth >= 800){
         });
     });
 }else{
+
+    // If screen-width is smaller than 800 pixel
+
     elements.forEach(el => {
         document.getElementById(el).classList.add('fadeIn');
     });
 }
 
-var timeLine = [
-    {
-        id: "text_1",
-        anim: "fadeIn",
-        time: 200
-    },
-    {
-        id: "text_1",
-        anim: "fadeOut",
-        time: 3200
-    },
-    {
-        id: "text_2",
-        anim: "fadeIn",
-        time: 4200
-    },
-    {
-        id: "text_2",
-        anim: "fadeOut",
-        time: 7200
-    },
-    {
-        id: "background",
-        anim: "fadeOut",
-        time: 7200
-    },
-    {
-        id: "container",
-        anim: "display",
-        time: 8200
-    },
-    {
-        id: "background",
-        anim: "hide",
-        time: 8200
-    },
-];
+// Run timeline and call animation function for each element in timeline
 
 timeLine.forEach(el =>  {
     animate(el['id'],el['anim'],el['time']);
